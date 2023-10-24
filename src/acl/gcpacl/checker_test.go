84f44640a36e9068bc5c77200e1421c056031868
lds <lds@nianticlabs.com>
2023-10-24 08:34:10 +0000

    Skip tests that need GCP credentials.

--- src/acl/gcpacl/checker_test.go
+++ src/acl/gcpacl/checker_test.go
@@ -26,6 +26,10 @@ func TestMain(m *testing.M) {
 }
 
 func TestInvalidNoToken(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode: need GCP credentials")
+	}
+
 	ctx := context.Background()
 	storage := memstorage.New()
 	gcpCollector := gcpcollector.NewFake(ctx, storage)
@@ -41,6 +45,9 @@ func TestInvalidNoToken(t *testing.T) {
 }
 
 func TestInvalidParseToken(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode: need GCP credentials")
+	}
 	ctx := context.Background()
 	ctx = metadata.NewIncomingContext(ctx, metadata.New(
 		map[string]string{"Authorization": "QxMzZjMjAyYjhkMjkwNTgwYjE2NWMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF6cCI6InNlcnZpY2VhY2NvdW50bmFtZUBzZWMtZXNhbGltYmVuaS1hcGkta2V5LXRlc3QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJlbWFpbCI6InNlcnZpY2VhY2NvdW50bmFtZUBzZWMtZXNhbGltYmVuaS1hcGkta2V5LXRlc3QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZXhwIjoxNjU5NTIxODcxLCJpYXQiOjE2NTk1MTgyNzEsImlzcyI6Imh0dHBzOi8vYWNjb3VudHMuZ29vZ2xlLmNvbSIsInN1YiI6IjEwNTM5OTA4NzA5OTk1NDg5MDA0MyJ9.LHwLeuAa6jZc7pFPhtFLvsKMg56vPHrm83dfVukLxycopz6CzkpFZoF_DXFRKQ-myQs4KMMd44loi29te5vfnAL9aMXwvySFEcjESOIE_SXPND3Q5FBlfRfWoSLFjGsGhqLhNwKQn-tvjynkdxmtopL4qVmhAFpgTqNA4u8b7l7cWsl3zoudPZMy8mi5pIUetWH5jpj7OaPyv9pVaQ-LaXaLUQkD8bx0bL3Tjhu9yu2IP2Z06jFR9mN-fJ60F05kMJ6Y4HquDXNjm8HCNrXfMGHBcKMUzE3wAaOIG4PoGI81t43dPWpUIUg07RS5tG5uxuWIrgJddxJYpYCWOhGjog"}))
